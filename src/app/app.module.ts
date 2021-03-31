15237a72083f2e8b49a3d3a2e475d6780a71cb0a
varad <varadkhokle10@gmail.com>
2021-03-31 14:55:18 +0530

    done
    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
+import { AddUserComponent } from './add-user/add-user.component';
+import { AllUsersComponent } from './all-users/all-users.component';
+import { CovidMeterComponent } from './covid-meter/covid-meter.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule,ReactiveFormsModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, AddUserComponent, AllUsersComponent, CovidMeterComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/covid-meter/covid-meter.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CovidMeterComponent } from './covid-meter.component';
+
+describe('CovidMeterComponent', () => {
+  let component: CovidMeterComponent;
+  let fixture: ComponentFixture<CovidMeterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CovidMeterComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CovidMeterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
